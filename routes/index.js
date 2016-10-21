b2897c5d0e8ec1c4820fbdda95bcc1428f8752f1
koconnor29 <kevinoconnor29@gmail.com>
2016-10-21 15:10:40 -0700

    fixed bug

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,12 @@ exports.index = function(req, res) {
   res.render('index');
 };
 
+exports.grader = function(req, res) {
+  console.log(req);
+  console.log(req.body);
+
+  res.end(true);
+};
+
+
+
