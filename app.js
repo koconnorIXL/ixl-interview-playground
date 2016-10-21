73425f9cf3fe57d840bbc2814de6449a5f81dc55
Jerry Lung <jlung@ixl.com>
2016-10-21 15:02:32 -0700

    Buttons ex1

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var routes = require('./routes')
 var path = require('path');
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, '/scripts')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', routes.index);
 
